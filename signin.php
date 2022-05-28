d237b0dd69844af58b69211edd219bdced04e6b9
SamAbdallah <saa081@student.bau.edu.lb>
2022-05-28 12:59:35 +0300

    adding condition to check is user exists

--- signin.php
+++ signin.php
@@ -8,3 +8,14 @@ $second=$_POST["password"];
 
 $sql="select * from users where email='".$first."' AND password='".$password"' limit 1";  
 $login=$mysqli($sql);
+
+if (mysql_num_rows($login)==1) {
+    echo "You have successfully logged in!";
+    exit();
+   }
+   
+   
+   else {
+    echo "You have entered wrong username or password" ;
+    exit();
+   }
