cb14041de91c0e8578e8cf6c3fc82ebfe3f59aaa
SamAbdallah <saa081@student.bau.edu.lb>
2022-05-28 12:55:53 +0300

    adding sql query for sign in

--- signin.php
+++ signin.php
@@ -5,3 +5,6 @@ include("connection.php");
 
 $first=$_POST["email"];
 $second=$_POST["password"];
+
+$sql="select * from users where email='".$first."' AND password='".$password"' limit 1";  
+$login=$mysqli($sql);
