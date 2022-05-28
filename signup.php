4bc6870aeef8283faf231b7e3411a96343bd1ddd
SamAbdallah <saa081@student.bau.edu.lb>
2022-05-28 12:52:27 +0300

    adding sql queries for sign up

---
+++ signup.php
@@ -0,0 +1,18 @@
+<?php
+
+include("connection.php");
+
+
+$first=$_POST["sname"];
+$second=$_POST["fname"];
+$third=$_POST["phone_number"];
+$fourth=$_POST["email"];
+$sixth=$_POST["type"];
+$seventh=$_POST["city"];
+$fifth = $_POST["password"];
+
+$hashed_password=password_hash($fifth,PASSWORD_DEFAULT);
+
+$query=$mysqli ->prepare("INSERT INTO users(sname,fname,phone_number,email,type,city,password) VALUES (?,?,?,?,?,?,?)");
+$query ->bind_param("ssissss",$first,$second,$third,$fourth,$sixth,$seventh,$hashed_password);
+$query ->execute();
